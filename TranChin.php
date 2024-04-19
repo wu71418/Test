b527a2ca358a220a8b044d8f98f22f9366d065cc
wu71418 <wu71418@gmail.com>
2024-04-19 16:23:19 +0800

    修改細節

--- TranChin.php
+++ TranChin.php
@@ -2,7 +2,7 @@
     $Number = $_POST['Number'];
     
     $Array_Number = array('零','壹','貳','參','肆','伍','陸','柒','捌','玖');
-    $Array_Unit = array('元','拾','佰','仟','萬','拾萬','佰萬','仟萬','億','拾億');
+    $Array_Unit = array('元','拾','佰','仟','萬','拾萬','佰萬','仟萬','億');
 
     $result = '';
     $str_length = strlen($Number);
